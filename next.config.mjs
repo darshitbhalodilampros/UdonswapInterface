c94c8cc01569002700fa75f4015649c237c23427
darshitbhalodilampros <darshitbhalodia@lampros.tech>
2024-06-28 21:05:39 +0530

    return to default config

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    images: {
-      unoptimized: true,
-    },
-    trailingSlash: true,
-    webpack: (config) => {
-      // Add file-loader to handle mp4 files
-      config.module.rules.push({
+// next.config.js
+
+import path from 'path';
+
+export function webpack(config) {
+    // Add file-loader to handle mp4 files
+    config.module.rules.push({
         test: /\.mp4$/,
         use: {
-          loader: 'file-loader',
-          options: {
-            outputPath: 'static/media',
-            name: '[name].[ext]'
-          }
+            loader: 'file-loader',
+            options: {
+                outputPath: 'static/media',
+                name: '[name].[ext]'
+            }
         }
-      });
-      return config;
-    }
-  };
-  
-  module.exports = nextConfig;
-  
+    });
+
+    return config;
+}
+
+
