47e3b23b2cbbb118aeac860de9d1965484add754
darshitbhalodilampros <darshitbhalodia@lampros.tech>
2024-06-28 20:53:21 +0530

    config rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-// next.config.js
-
-import path from 'path';
-
-function webpack(config) {
-    // Add file-loader to handle mp4 files
-    config.module.rules.push({
-        test: /\.mp4$/,
-        use: {
-            loader: 'file-loader',
-            options: {
-                outputPath: 'static/media',
-                name: '[name].[ext]'
-            }
-        }
-    });
-    return config;
-}
-
 const nextConfig = {
     output: 'export',
     reactStrictMode: true,
@@ -25,6 +6,21 @@ const nextConfig = {
       unoptimized: true,
     },
     trailingSlash: true,
-  }
-
-module.exports = {nextConfig, webpack}
+    webpack: (config) => {
+      // Add file-loader to handle mp4 files
+      config.module.rules.push({
+        test: /\.mp4$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'static/media',
+            name: '[name].[ext]'
+          }
+        }
+      });
+      return config;
+    }
+  };
+  
+  module.exports = nextConfig;
+  
